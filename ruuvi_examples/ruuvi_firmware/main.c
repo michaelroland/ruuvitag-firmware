ruuvi/ruuvitag_fw

commit 36f01269ee3bceb5105e93ee34febc6c2234f009

don't init RTC to save power

ruuvi_examples/ruuvi_firmware/main.c
@@ -180,7 +180,7 @@ void main_timer_handler(void * p_context)
       raw_h = bme280_get_humidity();
       
       // Start next measurement - causes up to URL_LOOP_INTERVAL latency in measurements.
-      bme280_set_mode(BME280_MODE_FORCED);
+      //bme280_set_mode(BME280_MODE_FORCED);
 
       // Get accelerometer data.
       lis2dh12_read_samples(&buffer, 1);  
@@ -189,7 +189,7 @@ void main_timer_handler(void * p_context)
       acc[2] = buffer.sensor.z;
     }
     // If only temperature sensor is present.
-    else 
+    else
     {
       int32_t temp;                                        // variable to hold temp reading
       (void)sd_temp_get(&temp);                            // get new temperature
@@ -197,9 +197,10 @@ void main_timer_handler(void * p_context)
       raw_t = (int32_t) temp;
     }
 
-    // Get battery voltage.
+    // Get battery voltage every 30.th cycle
+    static uint32_t vbat_update_counter;
     static uint16_t vbat = 0;
-    vbat = getBattery();
+    if(!(vbat_update_counter++%30)) { vbat = getBattery(); }
     //NRF_LOG_INFO("temperature: , pressure: , humidity: ");
     // Embed data into structure for parsing.
     parseSensorData(&data, raw_t, raw_p, raw_h, vbat, acc);
@@ -269,7 +270,7 @@ int main(void)
   err_code |= init_timer(main_timer_id, MAIN_LOOP_INTERVAL_RAW, main_timer_handler);
 
   // Initialize RTC.
-  err_code |= init_rtc();
+  //err_code |= init_rtc();
 
   // Start interrupts.
   err_code |= pin_interrupt_init();
@@ -317,11 +318,12 @@ int main(void)
     lis2dh12_set_interrupts(LIS2DH12_I2C_INT2_MASK, 2);
 
     // Setup BME280 - oversampling must be set for each used sensor.
-    bme280_set_oversampling_hum(BME280_OVERSAMPLING_16);
-    bme280_set_oversampling_temp(BME280_OVERSAMPLING_16);
-    bme280_set_oversampling_press(BME280_OVERSAMPLING_16);
+    bme280_set_oversampling_hum(BME280_OVERSAMPLING_1);
+    bme280_set_oversampling_temp(BME280_OVERSAMPLING_1);
+    bme280_set_oversampling_press(BME280_OVERSAMPLING_1);
     bme280_set_iir(BME280_IIR_16);
-    bme280_set_mode(BME280_MODE_FORCED);
+    bme280_set_interval(BME280_STANDBY_1000_MS);
+    bme280_set_mode(BME280_MODE_NORMAL);
     NRF_LOG_DEBUG("BME280 configuration done\r\n");
     highres = true;
   }

michaelroland/ruuvitag-firmware

commit 9b115fafee43630174fa35fc58119053b0cba93e

Fix button press detection to allow debounce with disabled RTC (regression from commit 36f01269ee3bceb5105e93ee34febc6c2234f009)

ruuvi_examples/ruuvi_firmware/main.c
@@ -60,6 +60,7 @@
 
 // Constants
 #define DEAD_BEEF               0xDEADBEEF    //!< Value used as error code on stack dump, can be used to identify stack location on stack unwind.
+#define USE_RTC 0
 
 // ID for main loop timer.
 APP_TIMER_DEF(main_timer_id);                 // Creates timer id for our program.
@@ -70,15 +71,52 @@ APP_TIMER_DEF(main_timer_id);                 // Creates timer id for our progra
 #define BATTERY_INTERVAL_RAW_LOW_MASK 0x03F
 #define MAIN_LOOP_INTERVAL_RAW_ACC 2500u
 #define BATTERY_INTERVAL_RAW_ACC_MASK 0x07F
-#define DEBOUNCE_THRESHOLD 250u
+#define DEBOUNCE_TRESHOLD 250u
 
 static uint8_t data_buffer[24] = { 0 };
 static bool model_plus = false;     // Flag for sensors available
 static bool highres = false;        // Flag for used mode
-static uint64_t debounce = false;   // Flag for avoiding double presses
 static uint32_t acceleration_events = 0;
 
+#if USE_RTC > 0
+static uint64_t debounce = 0;       // Time for avoiding double presses
+#else
+static bool debounce = false;       // Flag for avoiding double presses
+#endif
+
 static void main_timer_handler(void * p_context);
+
+
+#if USE_RTC > 0
+static inline bool debounce_ignore(void)
+{
+    uint64_t now = millis();
+    if ((now - debounce) < DEBOUNCE_THRESHOLD) {
+        return true;
+    }
+    debounce = now;
+    return false;
+}
+
+static inline void debounce_clear(void)
+{
+    // no need to clear since debounce uses RTC
+}
+#else
+static inline bool debounce_ignore(void)
+{
+    if (debounce) {
+        return true;
+    }
+    debounce = true;
+    return false;
+}
+
+static inline void debounce_clear(void)
+{
+    debounce = false;
+}
+#endif
 
 
 /**@brief Handler for button press.
@@ -87,8 +125,7 @@ static void main_timer_handler(void * p_context);
 void change_mode(void* data, uint16_t length)
 {
   // Avoid double presses
-  if ((millis() - debounce) < DEBOUNCE_THRESHOLD) { return; }
-  debounce = millis();
+  if (debounce_ignore()) { return; }
   highres = !highres;
   if (model_plus)
   {
@@ -165,6 +202,8 @@ void main_timer_handler(void * p_context)
     bme280_data_t environmental;
     lis2dh12_sensor_buffer_t buffer;
     
+    debounce_clear();
+
     // If we have all the sensors.
     if (model_plus) {
         // Get environmental data.
@@ -269,8 +308,10 @@ int main(void)
   bluetooth_configure_advertising_interval(MAIN_LOOP_INTERVAL_RAW_ACC); // Broadcast only updated data, assuming there is an active receiver nearby.
   
   // Initialize RTC.
-  //err_code |= init_rtc();
-
+  #if USE_RTC > 0
+  err_code |= init_rtc();
+  #endif
+  
   // Start interrupts.
   err_code |= pin_interrupt_init();
   // Initialize button.