michaelroland/ruuvitag-firmware

commit 93a4ef6075e251bdaf1e769521684fc90a5573d0

Start BLE advertisements on startup and do not restart on data updates (adapt to changes from commit 0a2c10d18d737af6e0e1bdd41092cd4c9f438236)

ruuvi_examples/ruuvi_firmware/main.c
@@ -150,10 +150,9 @@ static void power_manage(void)
 
 static void updateAdvertisement(void)
 {
-  ret_code_t err_code = NRF_SUCCESS;
-  err_code |= bluetooth_set_manufacturer_data(data_buffer, sizeof(data_buffer));
+  ret_code_t err_code;
+  err_code = bluetooth_set_manufacturer_data(data_buffer, sizeof(data_buffer));
   NRF_LOG_DEBUG("Applying configuration, data status %d\r\n", err_code);
-  err_code |= bluetooth_apply_configuration();
 }
 
 
@@ -268,7 +267,7 @@ int main(void)
   // Initialize the application timer module.
   err_code |= init_timer(main_timer_id, MAIN_LOOP_INTERVAL_RAW_ACC, main_timer_handler);
   bluetooth_configure_advertising_interval(MAIN_LOOP_INTERVAL_RAW_ACC); // Broadcast only updated data, assuming there is an active receiver nearby.
-
+  
   // Initialize RTC.
   //err_code |= init_rtc();
 
@@ -339,6 +338,9 @@ int main(void)
   // Init ok, start watchdog with default wdt event handler (reset).
   init_watchdog(NULL);
 
+  // apply BT configuration
+  bluetooth_apply_configuration();
+  
   // Enter main loop.
   for (;;)
   {

commit 3f6b5975e99ff69f1955025a3b259ee3b6184ebc

Fix update of BLE advertisement interval (regression from commit 93a4ef6075e251bdaf1e769521684fc90a5573d0)

ruuvi_examples/ruuvi_firmware/main.c
@@ -148,7 +148,7 @@ void change_mode(void* data, uint16_t length)
     }
   }
   NRF_LOG_INFO("Updating in %d mode\r\n", (uint32_t) highres);
-  main_timer_handler(NULL);
+  bluetooth_apply_configuration();
 }
 
 